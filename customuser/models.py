d3fadbbe44d58521e8384664b760083d39f647d9
iver56 <iver56@hotmail.com>
2014-09-25 11:05:57 +0200

    Remove customuser model

--- customuser/models.py
+++ customuser/models.py
@@ -1,12 +1,1 @@
 from django.db import models
-
-
-from django.contrib.auth.models import (
-    BaseUserManager, AbstractBaseUser
-)
-
-
-class MyUser(AbstractBaseUser):
-    identifier = models.CharField(max_length=40, unique=True)
-    patient = models.ForeignKey('patient.Patient')
-    USERNAME_FIELD = 'identifier'
