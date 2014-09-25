479799dc594a8578665b2233b6f5b2ded500abe5
Sigurd Sandve <zelspawn@gmail.com>
2014-09-25 13:37:28 +0200

    added help text to some models, and changed date_of_birth to national identification number

--- patient/models.py
+++ patient/models.py
@@ -4,7 +4,9 @@ from django.contrib.auth.models import User
 
 class Patient(models.Model):
     user = models.OneToOneField(User, null=True)
-    date_of_birth = models.DateField(null=False)
+    national_identification_number = models.PositiveSmallIntegerField(
+        max_length=11, help_text=
+        "A number consisting of date of birth + national id, called fødselsnummer, needs to be 11 digits long")
     telephone = models.IntegerField(default=None)
     address = models.TextField(default='')
     pulse_max = models.IntegerField(default=0)
@@ -15,7 +17,11 @@ class Patient(models.Model):
     temperature_min = models.IntegerField(default=0)
     activity_max = models.IntegerField(default=0)
     activity_min = models.IntegerField(default=0)
-    activity_access = models.BooleanField(default=False)
-    pulse_access = models.BooleanField(default=False)
-    o2_access = models.BooleanField(default=False)
-    temperature_access = models.BooleanField(default=False)
+    activity_access = models.BooleanField(
+        default=False, help_text="If True, the patient has access to view activity data in patient interface")
+    pulse_access = models.BooleanField(
+        default=False, help_text="If True, the patient has access to view pulse data in patient interface")
+    o2_access = models.BooleanField(
+        default=False, help_text="If True, the patient has access to view o2 data in patient interface")
+    temperature_access = models.BooleanField(
+        default=False, help_text="If True, the patient has access to view temperature data in patient interface")
