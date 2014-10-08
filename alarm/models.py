8b0c698915e3be9a295afe90e7aa92f91b49ec9d
Sigurd Sandve <sigurdsandve@gmail.com>
2014-10-08 15:14:37 +0200

    added measurement to alarm foreign key

--- alarm/models.py
+++ alarm/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from measurement.models import Measurement
 
 
 class Alarm(models.Model):
 
+    measurement = models.ForeignKey(Measurement)
     time_created = models.DateTimeField(null=False)
     is_treated = models.BooleanField(default=False, null=False)
     treated_text = models.TextField(default="", blank=True)
