097eb8ed3d7272d8f8693b9143a834159c309e1f
Tan Le <tanql@stud.ntnu.no>
2014-10-08 13:02:08 +0200

    endpoint patient

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework import routers
 from alarm import views as alarm_views
+from patient import views as patient_views
 
 router = routers.DefaultRouter()
 router.register(r'alarms', alarm_views.AlarmViewSet)
+router.register(r'patients',patient_views.PatientViewSet)
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
