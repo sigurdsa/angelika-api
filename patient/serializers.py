77c5bac9d7d2ec1d901b98491efb26246d46c218
iver56 <iver56@hotmail.com>
2014-10-10 12:25:45 +0200

    Add name, age and birth date in the patient list

--- patient/serializers.py
+++ patient/serializers.py
@@ -1,22 +1,51 @@
+from django.contrib.auth.models import User
 from .models import Patient
 from rest_framework import serializers
+import datetime
 
 
-class PatientSerializer(serializers.ModelSerializer):
+class SimpleUserSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField('get_full_name')
+
+    class Meta:
+        model = User
+        fields = [
+            'full_name',
+        ]
+
+    def get_full_name(self, obj):
+        return obj.get_full_name()
+
+
+class PatientListSerializer(serializers.ModelSerializer):
+    user = SimpleUserSerializer()
+    birth_date = serializers.SerializerMethodField('get_birth_date')
+    age = serializers.SerializerMethodField('get_age')
+
+    def get_birth_date(self, obj):
+        return obj.national_identification_number[0:2] + "."\
+            + obj.national_identification_number[2:4] + "."\
+            + obj.national_identification_number[4:6]
+
+    def get_age(self, obj):
+        today = datetime.datetime.today()
+        ddmm = obj.national_identification_number[0:4]
+        yyyy = "20" + obj.national_identification_number[4:6]
+        if int(yyyy) >= today.year:
+            yyyy = str(int(yyyy) - 100)
+        birth_date = datetime.datetime.strptime(ddmm + yyyy, "%d%m%Y")
+        diff = today - birth_date
+        num_years = int(diff.days / 365.2425)  # rough estimate, can be wrong in some edge cases
+        return num_years
+
     class Meta:
         model = Patient
-        fields = ('national_identification_number',
-    'telephone',
-    'address',
-    'pulse_max',
-    'pulse_min',
-    'o2_max',
-    'o2_min',
-    'temperature_max',
-    'temperature_min',
-    'activity_max',
-    'activity_min',
-    'activity_access',
-    'pulse_access',
-    'o2_access',
-    'temperature_access')
+        fields = (
+            'user',
+            'birth_date',
+            'age',
+            'national_identification_number',
+            'telephone'
+        )
+
+
--- patient/views.py
+++ patient/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
 from .models import Patient
 from rest_framework import viewsets
-from .serializers import PatientSerializer
+from .serializers import PatientListSerializer
 
 
 class PatientViewSet(viewsets.ModelViewSet):
@@ -9,4 +8,4 @@ class PatientViewSet(viewsets.ModelViewSet):
     API endpoint that shows all alarms
     """
     queryset = Patient.objects.all()
-    serializer_class = PatientSerializer
+    serializer_class = PatientListSerializer
