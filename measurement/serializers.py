0cf861a4a31700bf197d99442cd3966464dd522c
iver56 <iver56@hotmail.com>
2014-10-13 12:40:50 +0200

    Time as milliseconds since epoch

--- measurement/serializers.py
+++ measurement/serializers.py
@@ -1,8 +1,14 @@
 from .models import Measurement
 from rest_framework import serializers
+from calendar import timegm
 
 
 class MeasurementSerializer(serializers.ModelSerializer):
+    time = serializers.SerializerMethodField('get_time')
+
     class Meta:
         model = Measurement
         fields = ('time', 'value')
+
+    def get_time(self, obj):
+        return int(timegm(obj.time.utctimetuple())) * 1000  # Milliseconds since epoch, UTC
