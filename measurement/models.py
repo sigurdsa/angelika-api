282bff580b0efedf58fcdf52e32e193b6dd8e007
iver56 <iver56@hotmail.com>
2014-10-13 12:13:44 +0200

    Implement filter measurement by type

--- measurement/models.py
+++ measurement/models.py
@@ -3,19 +3,19 @@ from patient.models import Patient
 
 
 class Measurement(models.Model):
-    patient = models.ForeignKey(Patient)
-    value = models.FloatField(default=0)
+    patient = models.ForeignKey(Patient, null=False)
+    value = models.FloatField(default=0.0, null=False)
     time = models.DateTimeField(null=False)
 
-    ACTIVITY = 1
-    O2 = 2
-    PULSE = 3
-    TEMPERATURE = 4
+    ACTIVITY = 'A'
+    O2 = 'O'
+    PULSE = 'P'
+    TEMPERATURE = 'T'
     TYPES = [
         (ACTIVITY, 'Activity'),
         (O2, 'O2'),
         (PULSE, 'Pulse'),
         (TEMPERATURE, 'Temperature')
     ]
-    type = models.IntegerField(choices=TYPES, null=False)
+    type = models.CharField(max_length=1, choices=TYPES, null=False)
 
--- measurement/views.py
+++ measurement/views.py
@@ -1,7 +1,7 @@
 from .models import Measurement
 from rest_framework import viewsets
 from .serializers import MeasurementSerializer
-from django.contrib.auth.models import User
+from rest_framework.exceptions import ParseError
 
 
 class MeasurementViewSet(viewsets.ModelViewSet):
@@ -11,8 +11,16 @@ class MeasurementViewSet(viewsets.ModelViewSet):
     serializer_class = MeasurementSerializer
 
     def get_queryset(self):
-        queryset = Measurement.objects.none()
-        user_id = self.request.QUERY_PARAMS.get('user_id', None)
-        if user_id is not None:
-            queryset = queryset.filter(user__id=user_id)
+        patient_id = self.request.QUERY_PARAMS.get('patient_id', None)
+        if patient_id is None:
+            raise ParseError(detail="Query string 'patient_id' not specified")
+        if not patient_id.isdigit():
+            raise ParseError(detail="patient_id is not numeric")
+        type = self.request.QUERY_PARAMS.get('type', None)
+        if type is None:
+            raise ParseError(detail="Query string 'type' is not specified")
+        if not type in ['A', 'O', 'P', 'T']:
+            raise ParseError(detail="type must be one of the following values: 'A', 'O', 'P', 'T'")
+
+        queryset = Measurement.objects.filter(patient__id=patient_id, type=type)
         return queryset
