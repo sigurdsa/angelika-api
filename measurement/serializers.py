bd3f63238b0887fd8dafd06fcfdba6242f4890f1
iver56 <iver56@hotmail.com>
2014-10-13 12:55:29 +0200

    Highcharts compatible objects

--- measurement/serializers.py
+++ measurement/serializers.py
@@ -4,11 +4,15 @@ from calendar import timegm
 
 
 class MeasurementSerializer(serializers.ModelSerializer):
-    time = serializers.SerializerMethodField('get_time')
+    x = serializers.SerializerMethodField('get_time')
+    y = serializers.SerializerMethodField('get_value')
 
     class Meta:
         model = Measurement
-        fields = ('time', 'value')
+        fields = ('x', 'y')
 
     def get_time(self, obj):
         return int(timegm(obj.time.utctimetuple())) * 1000  # Milliseconds since epoch, UTC
+
+    def get_value(self, obj):
+        return obj.value
