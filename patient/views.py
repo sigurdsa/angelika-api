d58f7d34aea873810f2aa0a75d793d72cbe48d37
Tan Le <tanql@stud.ntnu.no>
2014-10-15 12:50:28 +0200

    Detailed patient fix

--- patient/views.py
+++ patient/views.py
@@ -1,11 +1,24 @@
 from .models import Patient
 from rest_framework import viewsets
 from .serializers import PatientListSerializer
+from .serializers import PatientDetailSerializer
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+
+
+
 
 
 class PatientViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that shows all alarms
-    """
-    queryset = Patient.objects.all()
-    serializer_class = PatientListSerializer
+    model=Patient
+    def list(self, request):
+        queryset = Patient.objects.all()
+        serializer_class = PatientListSerializer(queryset,many=True)
+        return Response(serializer_class.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Patient.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = PatientDetailSerializer(user)
+        return Response(serializer.data)
+
