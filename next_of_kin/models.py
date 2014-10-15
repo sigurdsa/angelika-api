2fffc04e3366c3bf66a4d7e1783fc381ec56cf3d
Sigurd Sandve <sigurdsandve@gmail.com>
2014-10-15 16:55:17 +0200

    name required field for nextofkin

--- next_of_kin/models.py
+++ next_of_kin/models.py
@@ -5,8 +5,8 @@ from django.utils.encoding import smart_unicode
 
 class NextOfKin(models.Model):
     patient = models.ForeignKey(Patient, null=False)
-    first_name = models.CharField(null=True, max_length=80)
-    last_name = models.CharField(null=True, max_length=80)
+    first_name = models.CharField(null=False, max_length=80)
+    last_name = models.CharField(null=False, max_length=80)
     address = models.CharField(max_length=250, blank=True)
     phone_number = models.CharField(max_length=16, null=True, blank=True)
     priority = models.IntegerField(default=0, null=False)
