f9de1341c39a7ff62230ad889b3a28ce389e64a2
iver56 <iver56@hotmail.com>
2014-10-21 15:42:35 +0200

    Implement CustomObtainAuthToken that returns role of user

--- api/urls.py
+++ api/urls.py
@@ -18,5 +18,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api-token-auth/', 'rest_framework.authtoken.views.obtain_auth_token'),
+    url(r'^api-token-auth/', 'token_auth.views.custom_obtain_auth_token'),
 )
