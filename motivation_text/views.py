70024f2e7fdb4c094df6e829bc11a437ef2d8a0f
iver56 <iver56@hotmail.com>
2014-10-22 12:45:19 +0200

    Remove viewsets for motivation_text and next_of_kin. These data will be included in the patient endpoint.

--- motivation_text/views.py
+++ motivation_text/views.py
@@ -1,11 +1,4 @@
 from .models import MotivationText
-from rest_framework import viewsets
 from .serializers import MotivationTextSerializer
-
-
-class MotivationTextViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that shows all MotivationText
-    """
-    queryset = MotivationText.objects.all()
-    serializer_class = MotivationTextSerializer
+from api.permissions import IsHealthProfessional, IsPatient
+from rest_framework.permissions import IsAuthenticated
