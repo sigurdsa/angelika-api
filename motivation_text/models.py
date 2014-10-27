4b23b2cb4fe97a2f7e15ead8b7890076feff4c6a
iver56 <iver56@hotmail.com>
2014-10-27 11:39:30 +0100

    Motivational text unicode

--- motivation_text/models.py
+++ motivation_text/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from patient.models import Patient
-
+from django.utils.encoding import smart_unicode
 
 class MotivationText(models.Model):
     patient = models.ForeignKey(Patient, null=False)
     text = models.TextField(default='', blank=False)
     time_created = models.DateTimeField(null=False, auto_now_add=True, auto_now=False)
+
+
+    def __unicode__(self):
+        return smart_unicode(
+            "Motivational text for " + self.patient.user.first_name + " " + self.patient.user.last_name
+            + " created at " + str(self.time_created)
+        )
 
     class Meta():
         ordering = ['-id']
