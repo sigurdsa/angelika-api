951e4960f178a6d19e1f191dcf54af00a73720e8
iver56 <iver56@hotmail.com>
2014-11-01 22:00:46 +0100

    Update STATIC_ROOT

--- api/settings.py
+++ api/settings.py
@@ -91,7 +91,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
