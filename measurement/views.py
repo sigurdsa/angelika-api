7173f678217d70d0f44f5c440a8c7a52aa4f4ab1
torgeha <torgeha@gmail.com>
2014-11-03 11:12:51 +0100

    time is tz-aware

--- measurement/views.py
+++ measurement/views.py
@@ -3,11 +3,14 @@ from rest_framework.exceptions import ParseError
 from api.permissions import IsPatient, IsHub
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
+from rest_framework import status
 from django.core.exceptions import PermissionDenied
 from rest_framework.response import Response
 from datetime import datetime
 from patient.models import Patient
 from patient.serializers import PatientGraphSeriesSerializer
+
+import pytz
 
 
 class CurrentPatientMeasurements(APIView):
@@ -34,67 +37,56 @@ class CurrentPatientMeasurements(APIView):
         return Response(serializer.data)
 
 class PostMeasurements(APIView):
-    permission_classes = (IsAuthenticated, IsHub) # TODO: Fix permissions, for example (IsAuthenticated, IsHub,)
+    permission_classes = (IsAuthenticated, IsHub, )
 
     def post(self, request, format=None):
+
         hub_id = request.DATA.get("Observation").get("hub_id")
 
-        # print "Measurements: ", request.DATA.get("Measurements")
-        # print "Measurements: "
-        # print request.DATA.get("Measurements")
+        patient = Patient.objects.get(hub__username=hub_id)
 
-        count = 0 # just to know how many measurements gets created
+        if patient:
+            patient_id = patient.id
+        else:
+            return Response({"hub_id could not be mapped to patient": -1}, status=status.HTTP_400_BAD_REQUEST)
+
+
+        count = 0 # How many measurements gets created?
 
         for measurement in request.DATA.get("Measurements"):
 
             date = measurement.get("date")
-            type = measurement.get("type") # only steps should be used for now, check this
+            m_type = measurement.get("type") # only steps should be used for now
             unit = measurement.get("unit")
             value = measurement.get("value")
 
             # Filter out all activities but steps
             ignored_types = ["elevation", "calories", "soft", "intense", "moderate", "distance"]
-            if type in ignored_types:
+            if m_type in ignored_types:
                 continue
 
             count += 1
 
             # Map hub-type --> model type
-            allowed_types = {"heart_rate" : 'P', "spo2": 'O', "steps": 'A'} # Maps value to model type
-            type = allowed_types[type]
+            allowed_types = {"heart_rate": 'P', "spo2": 'O', "steps": 'A'} # Maps value to model type
+            m_type = allowed_types[m_type]
 
             # Map hub-unit --> model unit
-            allowed_units = {"bpm": 'B', "percent" : 'E', 'steps': 'S', "m": 'M', 'kcal': 'K', 's': 'S'}
+            allowed_units = {"bpm": 'B', "percent": 'E', 'steps': 'S', "m": 'M', 'kcal': 'K', 's': 'S'}
             unit = allowed_units[unit]
 
             # Convert time format
             time = datetime.utcfromtimestamp(date)
 
-            # TODO map hub id to patient id!!!!
-            # patient_id = Patient.objects.all().first().id
-            patient = Patient.objects.get(hub_id=hub_id)
-
-            if patient:
-                patient_id = patient.id
-            else:
-                # TODO handle this!
-                pass
-
-
-            print "Will create measurement object with Time: ", time, "Type: ", type, "Value: ", value, "Unit: ", unit, " from hub: ", hub_id, "to patient_id: ", patient_id
-
+            # Time must be timezone-aware
+            tz_aware_time = time.replace(tzinfo=pytz.UTC)
 
             Measurement.objects.create(
                 patient_id=patient_id,
-                time=time,
-                type=type,
+                time=tz_aware_time,
+                type=m_type,
                 value=value,
                 unit=unit
             )
 
-            print "Posted..."
-            print "-----------------------------"
-        print "Measurements created: ", count
-
-
-        return Response({'WOHOO': True})
+        return Response({'Created measurements': count}, status=status.HTTP_201_CREATED)
