8f98eb65d817e3f1e93b623799641d07bb18c44f
iver56 <iver56@hotmail.com>
2014-11-04 13:38:10 +0100

    CORS allow all

--- api/settings.py
+++ api/settings.py
@@ -102,12 +102,7 @@ REST_FRAMEWORK = {
     )
 }
 
-CORS_ORIGIN_WHITELIST = (
-    'localhost:8080',
-    'iver.io',
-    'www.iver.io',
-    'folk.ntnu.no'
-)
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
 ALLOWED_HOSTS = ['localhost']
