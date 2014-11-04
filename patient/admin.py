1ee37dad2575a5ed49acc6c7ce66684d15bf5061
iver56 <iver56@hotmail.com>
2014-11-04 13:41:26 +0100

    In Django Patient admin, filter hub and user options

--- patient/admin.py
+++ patient/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from .models import Patient
+from django import forms
 
-admin.site.register(Patient)
+class CustomPatientForm(forms.ModelForm):
+    class Meta:
+        model = Patient
+
+    def __init__(self, *args, **kwargs):
+        super(CustomPatientForm, self).__init__(*args, **kwargs)
+        self.fields['hub'].queryset = Group.objects.get(name="hubs").user_set.all()
+        self.fields['user'].queryset = Group.objects.get(name="patients").user_set.all()
+
+
+class PatientAdmin(admin.ModelAdmin):
+    form = CustomPatientForm
+
+admin.site.register(Patient, PatientAdmin)
