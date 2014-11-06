337794fae61010434ace56cd31366ec2d8821762
iver56 <iver56@hotmail.com>
2014-11-06 20:17:25 +0100

    Improve performance @ alarm admin

--- alarm/admin.py
+++ alarm/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Alarm
 
-admin.site.register(Alarm)
+
+class AlarmAdmin(admin.ModelAdmin):
+    model = Alarm
+
+    def get_queryset(self, request):
+        return super(AlarmAdmin, self).get_queryset(request).select_related('measurement__patient__user')
+
+admin.site.register(Alarm, AlarmAdmin)
