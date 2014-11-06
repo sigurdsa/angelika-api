9bf829a38a17f9b532f403bf241aa5810ba720d6
iver56 <iver56@hotmail.com>
2014-11-06 21:13:13 +0100

    Improve performance of MotivationText admin

--- motivation_text/admin.py
+++ motivation_text/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import MotivationText
 
-admin.site.register(MotivationText)
+
+class MotivationTextAdmin(admin.ModelAdmin):
+    model = MotivationText
+
+    def get_queryset(self, request):
+        return super(MotivationTextAdmin, self).get_queryset(request).select_related('patient__user')
+
+admin.site.register(MotivationText, MotivationTextAdmin)
