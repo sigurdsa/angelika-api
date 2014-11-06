f4edd96e875c597a9afc99977941849f80c23198
iver56 <iver56@hotmail.com>
2014-11-06 22:00:34 +0100

    Improve performance of ThresholdValue admin

--- threshold_value/admin.py
+++ threshold_value/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import ThresholdValue
 
-admin.site.register(ThresholdValue)
+
+class ThresholdValueAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        return super(ThresholdValueAdmin, self).get_queryset(request).select_related('patient__user')
+
+admin.site.register(ThresholdValue, ThresholdValueAdmin)
