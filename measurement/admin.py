6cb0822aade07999d54e5fcd19eb2c7322abc80a
iver56 <iver56@hotmail.com>
2014-11-06 20:13:53 +0100

    Improve performance @ Measurement Admin

--- measurement/admin.py
+++ measurement/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Measurement
 
-admin.site.register(Measurement)
+
+class MeasurementAdmin(admin.ModelAdmin):
+    model = Measurement
+
+    def get_queryset(self, request):
+        return super(MeasurementAdmin, self).get_queryset(request).select_related('patient__user')
+
+admin.site.register(Measurement, MeasurementAdmin)
