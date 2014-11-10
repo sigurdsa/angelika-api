309fdec7ed917a3a6e38790722cfaeba6cb16bd5
iver56 <iver56@hotmail.com>
2014-11-10 16:43:15 +0100

    Filter measurement options in Alarm admin

--- alarm/admin.py
+++ alarm/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from .models import Alarm
+from django import forms
+from measurement.models import Measurement
+
+
+class CustomAlarmForm(forms.ModelForm):
+    class Meta:
+        model = Alarm
+        exclude = []
+
+    def __init__(self, *args, **kwargs):
+        super(CustomAlarmForm, self).__init__(*args, **kwargs)
+        self.fields['measurement'].queryset = Measurement.objects.filter(alarm__isnull=True, type__in=['O', 'P', 'T'])
 
 
 class AlarmAdmin(admin.ModelAdmin):
+    form = CustomAlarmForm
     model = Alarm
 
     def get_queryset(self, request):
