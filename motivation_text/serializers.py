b606eebc900645fdfbbf867c744ef6ea785c2e33
iver56 <iver56@hotmail.com>
2014-11-16 02:00:58 +0100

    Serialize MotivationText sound

--- motivation_text/serializers.py
+++ motivation_text/serializers.py
@@ -1,9 +1,17 @@
 from .models import MotivationText
 from rest_framework import serializers
+from rest_framework.serializers import SerializerMethodField
 
 
 class MotivationTextSerializer(serializers.ModelSerializer):
+    sound = SerializerMethodField('get_sound')
+
     class Meta:
         model = MotivationText
-        fields = ('id', 'time_created', 'text',)
+        fields = ('id', 'time_created', 'text', 'sound')
         read_only_fields = ('time_created',)
+
+    def get_sound(self, obj):
+        if obj.sound:
+            return {'url': self.context['request'].build_absolute_uri(obj.sound.url)}
+        return {}
